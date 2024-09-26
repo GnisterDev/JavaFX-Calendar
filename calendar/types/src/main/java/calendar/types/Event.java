075d117cc510594cca65f12c37ff273570d138e9
Igor Iwanicki <igor.iwanicki@protonmail.com>
2024-09-26 11:00:42 +0200

    Add Event type. Co-authored by: @bahp

--- calendar/types/src/main/java/calendar/types/Event.java
+++ calendar/types/src/main/java/calendar/types/Event.java
@@ -1,4 +1,50 @@
 package calendar.types;
 
-public class Event {
+import java.util.UUID;
+
+import javafx.scene.paint.Color;
+
+enum EventType {
+    EVENT_TYPE1,
+    EVENT_TYPE2
+}
+
+class Event {
+    private UUID id;
+    private EventType type;
+    private String title;
+    private String description;
+    private Color color;
+
+    Event(String title, String description) {
+        this(title, description, EventType.EVENT_TYPE1, Color.BLUE, UUID.randomUUID());
+    }
+
+    Event(String title, String description, EventType type, Color color, UUID id) {
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.color = color;
+        this.id = id;
+    }
+
+    UUID getId() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    EventType getType() {
+        return type;
+    }
+
+    Color getColor() {
+        return color;
+    }
 }
--- calendar/types/src/main/java/module-info.java
+++ calendar/types/src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module calendar.types {
+    requires javafx.graphics;
+
     exports calendar.types;
 }
