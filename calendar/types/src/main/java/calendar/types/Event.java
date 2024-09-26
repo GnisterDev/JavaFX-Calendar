5bc89d81456d129048b09965944fcd3d0b5f3f0c
Igor Iwanicki <igor.iwanicki@protonmail.com>
2024-09-26 11:33:01 +0200

    Add time to event type. Co-authored by: @sindrasc

--- calendar/types/src/main/java/calendar/types/Event.java
+++ calendar/types/src/main/java/calendar/types/Event.java
@@ -1,5 +1,6 @@
 package calendar.types;
 
+import java.util.Date;
 import java.util.UUID;
 
 import javafx.scene.paint.Color;
@@ -9,15 +10,19 @@ public class Event {
     private EventType type;
     private String title;
     private String description;
+    private Date startTime;
+    private Date endTime;
     private Color color;
 
-    public Event(String title, String description) {
-        this(title, description, EventType.EVENT_TYPE1, Color.BLUE, UUID.randomUUID());
+    public Event(String title, String description, Date startTime, Date endTime) {
+        this(title, description, startTime, endTime, EventType.EVENT_TYPE1, Color.BLUE, UUID.randomUUID());
     }
 
-    public Event(String title, String description, EventType type, Color color, UUID id) {
+    public Event(String title, String description, Date startTime, Date endTime, EventType type, Color color, UUID id) {
         this.title = title;
         this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.type = type;
         this.color = color;
         this.id = id;
@@ -33,6 +38,14 @@ public class Event {
 
     public String getDescription() {
         return description;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
     }
 
     public EventType getType() {
