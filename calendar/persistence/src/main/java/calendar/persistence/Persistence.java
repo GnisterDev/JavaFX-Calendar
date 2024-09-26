eb183443a4eb44f12b3d4d98cac069c21745ac5c
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2024-09-26 12:10:33 +0200

    Temp changes

--- calendar/persistence/src/main/java/calendar/persistence/Persistence.java
+++ calendar/persistence/src/main/java/calendar/persistence/Persistence.java
@@ -1,24 +1,45 @@
 package calendar.persistence;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import calendar.types.Event;
+import calendar.types.UserStore;
 
 public class Persistence {
-    public static void write(Event e) throws IOException {
+    public static void write(UserStore object) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
         FileOutputStream fileOutputStream = new FileOutputStream("post.json");
-        mapper.writeValue(fileOutputStream, e);
+        mapper.writeValue(fileOutputStream, object);
         fileOutputStream.close();
     }
 
-    public static void write(CalendarData c) {
+    public static <T> T read(Class<T> topClass) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
 
+        InputStream fileInputStream = new FileInputStream("post.json");
+        T object = mapper.readValue(fileInputStream, topClass);
+
+        return object;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, UUID> StU = new HashMap<>();
+        StU.put("Hello", UUID.randomUUID());
+
+        UserStore userStore = new UserStore(StU, new HashMap<>());
+        try {
+            write(userStore);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
