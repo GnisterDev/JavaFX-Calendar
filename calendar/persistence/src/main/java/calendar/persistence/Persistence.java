299cb134a6d6c63ef8c98d3b6be8cee7034676b6
Sindre Augustin Schärer <sscharer@online.no>
2024-09-30 20:47:07 +0200

    More tests for Persistence

--- calendar/persistence/src/main/java/calendar/persistence/Persistence.java
+++ calendar/persistence/src/main/java/calendar/persistence/Persistence.java
@@ -4,27 +4,25 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
-import calendar.types.User;
-import calendar.types.UserSettings;
-import calendar.types.UserStore;
 import javafx.scene.paint.Color;
 import calendar.persistence.internal.ColorDeserializer;
 import calendar.persistence.internal.ColorSerializer;
 import calendar.persistence.internal.UUIDDeserializer;
-import calendar.types.Calendar;
-import calendar.types.Event;
 
 public class Persistence {
+    public static String DEAFULT_FILE_PATH = "post.json";
+
     public static <T> void write(T object) throws IOException {
+        Persistence.write(object, Persistence.DEAFULT_FILE_PATH);
+    }
+
+    public static <T> void write(T object, String filepath) throws IOException {
         SimpleModule module = new SimpleModule();
         module.addSerializer(Color.class, new ColorSerializer());
 
@@ -32,12 +30,16 @@ public class Persistence {
         mapper.registerModule(module);
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        FileOutputStream fileOutputStream = new FileOutputStream("post.json");
+        FileOutputStream fileOutputStream = new FileOutputStream(filepath);
         mapper.writeValue(fileOutputStream, object);
         fileOutputStream.close();
     }
 
     public static <T> T read(Class<T> topClass) throws IOException {
+        return Persistence.read(topClass, Persistence.DEAFULT_FILE_PATH);
+    }
+
+    public static <T> T read(Class<T> topClass, String filepath) throws IOException {
         SimpleModule module = new SimpleModule();
         module.addDeserializer(Color.class, new ColorDeserializer());
         module.addDeserializer(UUID.class, new UUIDDeserializer());
@@ -46,48 +48,9 @@ public class Persistence {
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         mapper.registerModule(module);
 
-        InputStream fileInputStream = new FileInputStream("post.json");
+        InputStream fileInputStream = new FileInputStream(filepath);
         T object = mapper.readValue(fileInputStream, topClass);
 
         return object;
     }
-
-    // public static void main(String[] args) {
-
-    //     ArrayList<Event> e = new ArrayList<>();
-    //     e.add(new Event("title1", "description1", new Date(), new Date()));
-    //     e.add(new Event("title2", "description2", new Date(), new Date()));
-    //     ArrayList<Calendar> cl = new ArrayList<>();
-    //     cl.add(new Calendar(e));
-    //     cl.add(new Calendar(e));
-
-    //     UUID id = UUID.randomUUID();
-    //     User u = new User(id, "A", "1", cl, new UserSettings(id));
-
-    //     HashMap<String, UUID> StU = new HashMap<>();
-    //     StU.put("A", id);
-    //     StU.put("B", UUID.randomUUID());
-    //     StU.put("C", UUID.randomUUID());
-    //     StU.put("D", UUID.randomUUID());
-
-    //     HashMap<UUID, User> UtU = new HashMap<>();
-    //     UtU.put(id, u);
-    //     UtU.put(UUID.randomUUID(), new User("B", "2"));
-    //     UtU.put(UUID.randomUUID(), new User("C", "3"));
-    //     UtU.put(UUID.randomUUID(), new User("D", "4"));
-
-    //     UserStore userStore = new UserStore(StU, UtU);
-    //     try {
-    //         write(userStore);
-    //         System.out.println(
-    //                 read(UserStore.class)
-    //                         .getUser(id)
-    //                         .get()
-    //                         .getCalendar(1)
-    //                         .getEvent(0)
-    //                         .getStartTime());
-    //     } catch (IOException err) {
-    //         System.out.println(err);
-    //     }
-    // }
 }
--- calendar/persistence/src/test/java/calendar/persistence/PersistenceTest.java
+++ calendar/persistence/src/test/java/calendar/persistence/PersistenceTest.java
@@ -1,17 +1,27 @@
 package calendar.persistence;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import calendar.types.Event;
 import calendar.types.User;
 import calendar.types.UserSettings;
+import calendar.types.UserStore;
+import calendar.types.Calendar;;
 
 public class PersistenceTest {
 
@@ -28,9 +38,9 @@ public class PersistenceTest {
     @Test
     public void testWriteAndReadUser() throws IOException {
         User user = new User("testUser", "testPassword");
-        Persistence.write(user);
+        Persistence.write(user, TEST_FILE_PATH);
 
-        User readUser = Persistence.read(User.class);
+        User readUser = Persistence.read(User.class, TEST_FILE_PATH);
         assertEquals(user.getUserId(), readUser.getUserId());
         assertEquals(user.getUsername(), readUser.getUsername());
     }
@@ -38,9 +48,9 @@ public class PersistenceTest {
     @Test
     public void testWriteAndReadEvent() throws IOException {
         Event event = new Event("Title", "Description", new Date(), new Date());
-        Persistence.write(event);
+        Persistence.write(event, TEST_FILE_PATH);
 
-        Event readEvent = Persistence.read(Event.class);
+        Event readEvent = Persistence.read(Event.class, TEST_FILE_PATH);
         assertEquals(event.getDescription(), readEvent.getDescription());
         assertEquals(event.getStartTime(), readEvent.getStartTime());
         assertEquals(event.getEndTime(), readEvent.getEndTime());
@@ -53,17 +63,109 @@ public class PersistenceTest {
     @Test
     public void testInvalidClassThrowsIOExeption() throws IOException {
         User user = new User("testUser", "testPassword");
-        Persistence.write(user);
+        Persistence.write(user, TEST_FILE_PATH);
 
         assertThrows(IOException.class, () -> {
-            Persistence.read(Event.class);
+            Persistence.read(Event.class, TEST_FILE_PATH);
         });
     }
 
     @Test
     public void testReadThrowsIOExceptionWhenFileNotFound() {
         assertThrows(IOException.class, () -> {
-            Persistence.read(UserSettings.class);
+            Persistence.read(UserSettings.class, TEST_FILE_PATH);
         });
+    }
+
+    @Test
+    public void testWriteReadWithoutPath() throws IOException {
+
+        User user = new User("testUser", "testPassword");
+
+        File fileMock = Mockito.mock(File.class);
+        try (MockedStatic<Persistence> persistenceMock = Mockito.mockStatic(Persistence.class)) {
+            persistenceMock.when(() -> Persistence.write(user)).thenAnswer(invocation -> null);
+            persistenceMock.when(() -> Persistence.read(User.class)).thenReturn(user);
+
+            when(fileMock.exists()).thenReturn(true);
+            Persistence.write(user);
+
+            User readUser = Persistence.read(User.class);
+            assertEquals(user.getUserId(), readUser.getUserId());
+            assertEquals(user.getUsername(), readUser.getUsername());
+
+            if (!fileMock.exists())
+                throw new FileNotFoundException();
+            verify(fileMock, never()).delete();
+        }
+    }
+
+    @Test
+    public void testNestedClasses() throws IOException {
+        String username1 = "A";
+        String username2 = "B";
+        String username3 = "C";
+        String username4 = "D";
+
+        ArrayList<Event> eventList1 = new ArrayList<>();
+        eventList1.add(new Event("title1_1", "description1_1", new Date(), new Date()));
+        eventList1.add(new Event("title1_2", "description1_2", new Date(), new Date()));
+        ArrayList<Event> eventList2 = new ArrayList<>();
+        eventList2.add(new Event("title2_1", "description2_1", new Date(), new Date()));
+        eventList2.add(new Event("title2_2", "description2_2", new Date(), new Date()));
+        eventList2.add(new Event("title2_3", "description2_3", new Date(), new Date()));
+
+        ArrayList<Calendar> calendar = new ArrayList<>();
+        calendar.add(new Calendar(eventList1));
+        calendar.add(new Calendar(eventList2));
+
+        UUID user1ID = UUID.randomUUID();
+        UUID user2ID = UUID.randomUUID();
+        UUID user3ID = UUID.randomUUID();
+        UUID user4ID = UUID.randomUUID();
+
+        User User1 = new User(user1ID, username1, "password1", calendar, new UserSettings(user1ID));
+        User User2 = new User(user1ID, username2, "password2", new ArrayList<Calendar>(), new UserSettings(user1ID));
+        User User3 = new User(user1ID, username3, "password3", new ArrayList<Calendar>(), new UserSettings(user1ID));
+        User User4 = new User(user1ID, username4, "password4", new ArrayList<Calendar>(), new UserSettings(user1ID));
+
+        HashMap<String, UUID> StU = new HashMap<>();
+        StU.put(username1, user1ID);
+        StU.put(username2, user2ID);
+        StU.put(username3, user3ID);
+        StU.put(username4, user4ID);
+
+        HashMap<UUID, User> UtU = new HashMap<>();
+        UtU.put(user1ID, User1);
+        UtU.put(user2ID, User2);
+        UtU.put(user3ID, User3);
+        UtU.put(user4ID, User4);
+
+        UserStore userStore = new UserStore(StU, UtU);
+        Persistence.write(userStore, TEST_FILE_PATH);
+
+        UserStore object = Persistence.read(UserStore.class, TEST_FILE_PATH);
+
+        assertEquals(object.getClass(), UserStore.class);
+        assertTrue(Arrays.asList(username1, username2, username3, username4)
+                .stream()
+                .allMatch(name -> object.hasUsername(name)));
+        assertTrue(Arrays.asList(user1ID, user2ID, user3ID, user4ID)
+                .stream()
+                .allMatch(name -> object.hasUserId(name)));
+        assertEquals(object.getUser(user1ID)
+                .get()
+                .calendarCount(), 2);
+        assertEquals(object.getUser(user1ID)
+                .get()
+                .getCalendar(0)
+                .getEvents()
+                .size(), 2);
+        assertEquals(object.getUser(user1ID)
+                .get()
+                .getCalendar(1)
+                .getEvents()
+                .size(), 3);
+
     }
 }
