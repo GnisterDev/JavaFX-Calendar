4616662c6a3ee0b398bb35f23625632a841c23e2
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2024-10-10 10:35:21 +0200

    Merge ui-module into developmment

--- calendar/persistence/src/test/java/calendar/persistence/PersistenceTest.java
+++ calendar/persistence/src/test/java/calendar/persistence/PersistenceTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
 
