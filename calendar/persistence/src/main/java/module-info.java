52d64f053fe9cd9b5fe74a0b4a1558bcce5b880b
Gnister <sscharer@online.no>
2024-11-13 18:10:02 +0100

    replace calendar.types to javafx.graphics in module-info for persistance

--- calendar/persistence/src/main/java/module-info.java
+++ calendar/persistence/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module calendar.persistence {
-    requires transitive calendar.types;
+    requires transitive javafx.graphics;
     requires transitive com.fasterxml.jackson.annotation;
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.databind;
